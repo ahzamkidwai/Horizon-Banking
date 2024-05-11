30206b47d95df075baad470354532403a1dac303
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2024-05-12 00:15:03 +0530

    Implement Right Sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Ahzam" };
+  const loggedIn = {
+    firstName: "Ahzam",
+    lastName: "Kidwai",
+    email: "ahzamnaseem@gmail.com",
+  };
 
   return (
     <section className="home">
@@ -21,7 +26,13 @@ const Home = () => {
             totalCurrentBalance={1250.5}
           />
         </header>
+        {/* RECENT TRANSACTIONS */}
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, {currentBalance: 323.5}]}
+      />
     </section>
   );
 };
