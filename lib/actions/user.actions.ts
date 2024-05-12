1ae80012cb6a236dc9ffb1e22ce4f5679c88719f
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2024-05-13 00:49:48 +0530

    Implement Auth form for sign-in and sign-up

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,18 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+    // Mutation / Database / Make fetch
+  } catch (error) {
+    console.log("Error ", error);
+  }
+};
+
+export const signUp = async () => {
+    try {
+      // Mutation / Database / Make fetch
+    } catch (error) {
+      console.log("Error ", error);
+    }
+  };
+  
